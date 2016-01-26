9406bbaee8d5851a86b8188789e375d5c22ce326
Too <tootutor2014@gmail.com>
2016-01-26 13:12:54 +0700

    Replace tab with space

--- service/order/order-service.js
+++ service/order/order-service.js
@@ -1,21 +1,21 @@
 ttoApp.factory('OrderServ', function($resource) {
   return $resource(appInfo.apiPath + '/order/:orderId', {orderId: '@orderId'}, {
-		query: {
-			method: 'GET',
-			isArray: true
-		},	
-		update: {
-			method: 'POST',
-			headers: {
-				'X-HTTP-Method-Override' : 'PUT'
-			}
-		},
-		remove: {
-			method: 'POST',
-			headers: {
-				'X-HTTP-Method-Override' : 'DELETE'
-			}
-		}	
-	}); // Note the full endpoint address
-	
+    query: {
+      method: 'GET',
+      isArray: true
+    },  
+    update: {
+      method: 'POST',
+      headers: {
+        'X-HTTP-Method-Override' : 'PUT'
+      }
+    },
+    remove: {
+      method: 'POST',
+      headers: {
+        'X-HTTP-Method-Override' : 'DELETE'
+      }
+    } 
+  }); // Note the full endpoint address
+  
 });
