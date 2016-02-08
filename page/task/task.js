1db4f0c2dd038e1dd486d1ce18cc492166aa8f50
Too Tutor <tootutor2014@gmail.com>
2016-02-08 16:41:56 +0700

    updated task feature

--- page/task/task.js
+++ page/task/task.js
@@ -7,15 +7,27 @@ function ($scope, $rootScope, $routeParams, TaskServ) {
   $rootScope.component = {};
 
   $scope.userId = $routeParams.userId ? $routeParams.userId : $rootScope.userId;
+  $scope.processMode = $rootScope.processMode();
 
-  $rootScope.isLoading++;
-  $scope.taskList = TaskServ.query(
-    {sectionId: $routeParams.sectionId},
-    function (data) {
-      $rootScope.isLoading--;
-    }, function (response) {
-      $rootScope.errorDialog(response, 'Loading Error !!!');
-    }
-  );
-
+  if ($scope.processMode == 'user' || $scope.processMode == 'tutor') {
+    $rootScope.isLoading++;
+    $scope.taskList = TaskServ.query(
+      {userId: $scope.userId, sectionId: $routeParams.sectionId}, 
+      function (data) {
+        $rootScope.isLoading--;
+      }, function (response) {
+        $rootScope.errorDialog(response, 'Loading Error !!!');
+      }
+    );
+  } else {
+    $scope.taskList = TaskServ.query(
+      {sectionId: $routeParams.sectionId},
+      function (data) {
+        $rootScope.isLoading--;
+      }, function (response) {
+        $rootScope.errorDialog(response, 'Loading Error !!!');
+      }
+    );
+  }
+  
 }]);
