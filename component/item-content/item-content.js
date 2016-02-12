1e5c497b7818ff4caf72aa001bb61b0e189592b0
Too <tootutor2014@gmail.com>
2016-02-12 13:14:02 +0700

    Moved logic from item page to component

--- component/item-content/item-content.js
+++ component/item-content/item-content.js
@@ -1,6 +1,32 @@
 ttoApp.component('itemContent', {
   templateUrl: 'component/item-content/item-content.html',
-  bindings: {content: '='},
-  controller: function (ItemServ) {
+  bindings: {item: '='},
+  controller: function ($rootScope) {
+    this.updateChange = function () {
+      $rootScope.isLoading++;
+      if (this.item.itemId) {
+        this.item.$update(
+          function(data) {
+            $rootScope.isLoading--;
+            $rootScope.showToast('Updated successfully !!!');
+            this.item.editMode = false;
+          }, function (response) {
+            $rootScope.isLoading = 0;
+            $rootScope.errorDialog(response, 'Updating Error !!!');
+          }
+        );
+      } else {
+        this.item.$save(
+          function(data) {
+            $rootScope.isLoading--;
+            $rootScope.showToast('Added successfully !!!');
+            this.item.editMode = false;
+          }, function (response) {
+            $rootScope.isLoading = 0;
+            $rootScope.errorDialog(response, 'Adding Error !!!');
+          }
+        );
+      }
+    }
   }
 });
