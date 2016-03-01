16d3f09031db95b828b692dd480baf2b002e99b8
Too <tootutor2014@gmail.com>
2016-03-01 13:06:41 +0700

    Revised math display

--- libs/tto/ASCIIMathKaTeXHTML.js
+++ libs/tto/ASCIIMathKaTeXHTML.js
@@ -1,9 +1,23 @@
 function ASCIIMathKaTeXHTML(content) {
-  var regExp = /\$\$(.*?)\$\$/g;
-  return content.replace(regExp, getMath);
+  var regDisplayExp = /\$\$\n(.*?)\n\$\$/g;
+  var htmlDisplay = content.replace(regDisplayExp, getDisplayMath);
+  var regInlineExp = /\$(.*?)\$/g;
+  return htmlDisplay.replace(regInlineExp, getInlineMath);
 }
 
-function getMath(str, match1) {
+function getDisplayMath(str, match1) {
+	var kaTexStr = AMTparseAMtoTeX(match1);
+	var html = "KaTeX Error !!!";
+	try {
+		html = katex.renderToString(kaTexStr, { displayMode: true });
+	}
+	catch (err) {
+		html = err + ' : ' + match1;
+	}
+	return html;
+}
+
+function getInlineMath(str, match1) {
 	var kaTexStr = AMTparseAMtoTeX(match1);
 	var html = "KaTeX Error !!!";
 	try {
